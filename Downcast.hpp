12f6b610e919ec0ffff270ffaf42a674c50f7194
vicultraviolet <95176727+vicultraviolet@users.noreply.github.com>
2026-06-15 21:57:25 -0300

    Added `Downcast` function for `Ref`

---
+++ Downcast.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "./Ref.hpp"
+#include "./Option.hpp"
+
+namespace Nrl {
+    template<typename To, typename From>
+	[[nodiscard]] Option<Ref<To>> Downcast(Ref<From> from) {
+		To* casted = dynamic_cast<To*>((RemoveConst_t<RemovePointer_t<From*>>*)from.ptr());
+		if (!casted)
+		    return None();
+
+		return SomeWith<Ref<To>>(Ref<To>::New, *casted);
+	}
+} // namespace Nrl
