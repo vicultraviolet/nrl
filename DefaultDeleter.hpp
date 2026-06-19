6f6a40bd60448b39c8f171b8ef65c0c55febf965
vicultraviolet <95176727+vicultraviolet@users.noreply.github.com>
2026-06-18 23:13:57 -0300

    Renamed `ptr` in `DefaultDeleter::()` to `ref`

--- DefaultDeleter.hpp
+++ DefaultDeleter.hpp
@@ -8,10 +8,10 @@ namespace Nrl {
     public:
         [[nodiscard]] constexpr static DefaultDeleter New(void) { return DefaultDeleter(); }
 
-        void operator()(Ref<T> ptr) const noexcept {
+        void operator()(Ref<T> ref) const noexcept {
             auto allocator = Allocator<T>::New();
-            allocator.destroy(ptr);
-            allocator.dealloc(ptr, 1);
+            allocator.destroy(ref);
+            allocator.dealloc(ref, 1);
         }
     private:
         constexpr DefaultDeleter(void) = default;
