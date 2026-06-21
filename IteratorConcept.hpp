3576575530cbd1e4118957cfdfb942a864f5336c
vicultraviolet <95176727+vicultraviolet@users.noreply.github.com>
2026-06-21 15:26:24 -0300

    Updated iterator concepts

--- IteratorConcept.hpp
+++ IteratorConcept.hpp
@@ -7,10 +7,7 @@ namespace Nrl {
     concept c_InputIterator = c_Readable<It> && c_Incrementable<It> && c_EqualityComparable<It>;
 
     template<typename It>
-    concept c_ForwardIterator = c_InputIterator<It>;
-
-    template<typename It>
-    concept c_BidirectionalIterator = c_ForwardIterator<It> && c_Decrementable<It>;
+    concept c_BidirectionalIterator = c_InputIterator<It> && c_Decrementable<It>;
 
     template<typename It>
     concept c_RandomAccessIterator =
@@ -21,5 +18,6 @@ namespace Nrl {
             { it -= n } -> c_SameAs<It&>;
             { it +  n } -> c_SameAs<It>;
             { it -  n } -> c_SameAs<It>;
+            { it - it } -> c_SameAs<iptr>;
         };
 } // namespace Nrl
